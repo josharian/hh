e714847785402cb61e1aa3ddf74185ca8eb63eba
Josh Bleecher Snyder <josharian@gmail.com>
2025-02-03 14:04:25 -0800

    all: add buffering, improve api

--- hh.go
+++ hh.go
@@ -1,17 +1,16 @@
 package hh
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
 )
 
-// An HTTPResponseError is an error that specifies an HTTP status code and text to return to a client.
+// An HTTPResponseError is an error that can render itself as an HTTP response.
 type HTTPResponseError interface {
 	error
-	HTTPStatusCode() int
-	HTTPStatusText() string
+	RenderHTTP(w http.ResponseWriter)
 }
 
 // ResponseError is a convenience type that implements HTTPResponseError.
@@ -20,31 +19,37 @@ type ResponseError struct {
 	StatusText string // the text that accompanies the status code
 }
 
+var _ HTTPResponseError = (*ResponseError)(nil)
+
 func (e *ResponseError) Error() string {
 	return fmt.Sprintf("%d: %v", e.StatusCode, e.StatusText)
 }
-func (e *ResponseError) HTTPStatusCode() int    { return e.StatusCode }
-func (e *ResponseError) HTTPStatusText() string { return e.StatusText }
 
-// Error returns an HTTP error with status statusCode, with the default status text.
+func (e *ResponseError) RenderHTTP(w http.ResponseWriter) {
+	http.Error(w, e.StatusText, e.StatusCode)
+}
+
+// Error returns a ResponseError with status statusCode, with the default status text.
 func Error(statusCode int) error {
 	return &ResponseError{StatusCode: statusCode, StatusText: http.StatusText(statusCode)}
 }
 
-// ErrorText returns an HTTP error with status statusCode and text s.
+// ErrorText returns a ResponseError with status statusCode and text s.
 func ErrorText(statusCode int, s string) error {
 	return &ResponseError{StatusCode: statusCode, StatusText: s}
 }
 
-// Errorf returns an HTTP error with status statusCode and Sprintf-formatted text.
+// Errorf returns a ResponseError with status statusCode and Sprintf-formatted text.
 func Errorf(statusCode int, format string, args ...any) error {
 	return &ResponseError{StatusCode: statusCode, StatusText: fmt.Sprintf(format, args...)}
 }
 
-// ErrorJSON returns an HTTP error with status statusCode, accompanied by data encoded as JSON.
-// If data cannot be JSON-encoded, the response to the client
-// will be an HTTP 500 (Internal Server Error) with default 500 status text.
-// The error will contain details of the encoding failure.
+// ErrorJSON returns a ResponseError with status statusCode, accompanied by data encoded as JSON.
+// If data cannot be JSON-encoded, ErrorJSON returns an error created with fmt.Errorf.
+// In this case, the response to the client will be an HTTP 500 (Internal Server Error)
+// with default 500 status text, and the error will contain details of the encoding failure.
+// ErrorJSON does not set the Content-Type header.
+// To do that, implement a custom HTTPResponseError.
 func ErrorJSON(statusCode int, data any) error {
 	buf, err := json.Marshal(data)
 	if err != nil {
@@ -66,48 +71,135 @@ var (
 // A HandlerFunc is an http.HandlerFunc that returns an error. See Wrap.
 type HandlerFunc func(http.ResponseWriter, *http.Request) error
 
-// Wrap converts h to the standard http.HandlerFunc.
-// All errors returned by h are passed through the errorware, in order.
-// At that point, errors are converted to HTTP 500s (internal server error),
-// unless they implement HTTPResponseError, in which case the error specifies the status code and text.
+// Wrap converts h to a standard http.HandlerFunc.
 //
-// Wrap does not buffer output.
-// If you write to the response writer and then return an error,
-// returning the HTTP status code header will fail, and
-// the error text will be sent after the response writer output.
+// All errors returned by h are passed through the errorware, in order.
+// After errorware has been applied, non-nil errors are converted to HTTP 500s (internal server error),
+// unless they implement HTTPResponseError, or wrap an error that does,
+// in which case the error renders the response.
+//
+// Wrap buffers output and response headers until h returns.
+// This ensures that errors are correctly sent to the client.
+// For this reason, a wrapped handler's http.ResponseWriter
+// does not implement http.Flusher or http.Hijacker.
+// If this is not acceptable, do not use Wrap for this handler.
+// This package is designed to allow mix-and-match with non-error-returning handlers.
 func Wrap(h HandlerFunc, errorware ...func(*http.Request, error) error) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		err := h(w, r)
+		bufw := new(bufferingResponseWriter)
+		err := h(bufw, r)
+		if bufw.err != nil {
+			if err != nil {
+				err = fmt.Errorf("response write error (%v) after handler error: %w", bufw.err, err)
+			} else {
+				err = bufw.err
+			}
+		}
 		for _, fn := range errorware {
 			err = fn(r, err)
 		}
 		if err == nil {
+			bufw.flush(w)
 			return
 		}
-		re, ok := err.(HTTPResponseError)
-		if !ok {
+
+		re := asHTTPResponseError(err)
+		if re == nil {
+			// not an HTTPResponseError, convert to 500
 			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 			return
 		}
-		w.WriteHeader(re.HTTPStatusCode())
-		io.WriteString(w, re.HTTPStatusText())
+		re.RenderHTTP(w)
 	}
 }
 
-// Mux is a convenience wrapper around http.ServeMux.
-// Calls to Handle register a HandlerFunc with the wrapped mux.
-type Mux struct {
-	*http.ServeMux
-	errorware []func(*http.Request, error) error
+func asHTTPResponseError(err error) HTTPResponseError {
+	for {
+		if hre, ok := err.(HTTPResponseError); ok {
+			return hre
+		}
+		switch x := err.(type) {
+		case interface{ Unwrap() error }:
+			err = x.Unwrap()
+			if err == nil {
+				return nil
+			}
+			// continue outer for loop
+		case interface{ Unwrap() []error }:
+			for _, err := range x.Unwrap() {
+				if hre := asHTTPResponseError(err); hre != nil {
+					return hre
+				}
+			}
+			return nil
+		default:
+			return nil
+		}
+	}
 }
 
-// WrapMux creates a new Mux wrapping mux.
-// It applies errorware to each wrapped handler during registration.
-func WrapMux(mux *http.ServeMux, errorware ...func(*http.Request, error) error) *Mux {
-	return &Mux{ServeMux: mux, errorware: errorware}
+type bufferingResponseWriter struct {
+	header    http.Header
+	buffer    bytes.Buffer
+	code      int
+	wroteCode bool
+	wroteBody bool
+	err       error // Accumulate response writing errors
 }
 
-// HandleWrap registers handler for pattern.
-func (m *Mux) HandleWrap(pattern string, handler HandlerFunc) {
-	m.ServeMux.Handle(pattern, Wrap(handler, m.errorware...))
+func (w *bufferingResponseWriter) Header() http.Header {
+	if w.header == nil {
+		w.header = make(http.Header)
+	}
+	if w.wroteBody {
+		w.setError(ErrorText(http.StatusInternalServerError, "headers modified after being sent"))
+		// Still return a copy to prevent undefined behavior
+		h := make(http.Header, len(w.header))
+		for k, v := range w.header {
+			h[k] = v
+		}
+		return h
+	}
+	return w.header
+}
+
+func (w *bufferingResponseWriter) Write(b []byte) (int, error) {
+	if !w.wroteCode {
+		w.WriteHeader(http.StatusOK)
+	}
+	w.wroteBody = true
+	return w.buffer.Write(b)
+}
+
+func (w *bufferingResponseWriter) WriteHeader(code int) {
+	if w.wroteCode {
+		w.setError(ErrorText(http.StatusInternalServerError, "multiple calls to WriteHeader"))
+		return
+	}
+	if w.wroteBody {
+		w.setError(ErrorText(http.StatusInternalServerError, "WriteHeader called after Write"))
+		return
+	}
+	w.code = code
+	w.wroteCode = true
+}
+
+func (w *bufferingResponseWriter) setError(err error) {
+	if w.err == nil {
+		w.err = err
+	}
+}
+
+func (w *bufferingResponseWriter) flush(dst http.ResponseWriter) {
+	for k, v := range w.header {
+		dst.Header()[k] = v
+	}
+	if w.wroteCode {
+		dst.WriteHeader(w.code)
+	}
+	if w.buffer.Len() > 0 {
+		// intentionally ignore errors
+		// there's little we can do about them
+		_, _ = dst.Write(w.buffer.Bytes())
+	}
 }
--- readme.md
+++ readme.md
@@ -2,74 +2,77 @@ WARNING: Package hh is experimental while I tinker with the API.
 
 ---
 
-Package hh contains HTTP handler helpers.
+Package hh provides support for HTTP handlers that return errors.
 
 ```sh
 go get github.com/josharian/hh@latest
 ```
 
-Full discussion below. But most people just want examples, so...
+Full discussion below. But most people just want usage examples, so...
 
 # Examples
 
-Basic mux setup. Note that you can mix-and-match wrapped and unwrapped handlers.
+Basic usage. Note that you can mix-and-match wrapped and unwrapped handlers.
 
 ```go
-mux := hh.WrapMux(http.NewServeMux())
-mux.HandleFunc("GET /{$}", srv.handleRoot) // normal stdlib handler
-mux.HandleWrap("GET /thing/{id}", srv.handleThing) // wrapped handler
+mux.HandleFunc("GET /{$}", srv.handleRoot)                  // normal stdlib handler
+mux.HandleFunc("GET /thing/{id}", hh.Wrap(srv.handleThing)) // wrapped handler
 ```
 
-Basic handler usage, showing off lots of different error return options.
+Basic handler, showing off lots of different error return options.
 
 ```go
 func (s *HTTPServer) handleThing(w http.ResponseWriter, r *http.Request) error {
-    // This is toy code. Don't nitpick or copy unthinkingly, please!
-    id := r.PathValue("id")
-    thing, err := s.DB.LookUpThing(id)
-    if errors.Is(err, sql.ErrNoRows) {
-        return hh.ErrNotFound // returns a 404, with text "Not Found"
-    }
-    if err != nil {
-        return err // returns a 500, with text "Internal Server Error"
-    }
-    authorized := s.checkAuth(r, thing)
-    if !authorized {
-        return hh.ErrorText(http.StatusUnauthorized, "no thing for you") // returns 401, with custom text
-    }
-    if msg := thing.isBroken(); msg != nil {
-        return hh.Errorf(http.StatusServiceUnavailable, "this this is temporarily broken: %v", msg)
-    }
-    nextAllowedRequest := ratelimitDelay(r)
-    if delay := nextAllowedRequest.Sub(time.Now()); delay > 0 {
-        info := map[string]float64{"sleep_sec": delay.Seconds()}
-        return hh.ErrorJSON(http.StatusTooManyRequests, info) // return a 429 with JSON-encoded info
-    }
-    json.NewEncoder(w).Write(thing)
-    return nil
+	// This is toy code. Don't nitpick or copy unthinkingly, please!
+	id := r.PathValue("id")
+	thing, err := s.DB.LookUpThing(id)
+	if errors.Is(err, sql.ErrNoRows) {
+		return hh.ErrNotFound // returns a 404, with text "Not Found"
+	}
+	if err != nil {
+		return err // returns a 500, with text "Internal Server Error"
+	}
+	authorized := s.checkAuth(r, thing)
+	if !authorized {
+		return hh.ErrorText(http.StatusUnauthorized, "no thing for you") // returns 401, with custom text
+	}
+	if msg := thing.isBroken(); msg != nil {
+		return hh.Errorf(http.StatusServiceUnavailable, "this this is temporarily broken: %v", msg)
+	}
+	nextAllowedRequest := ratelimitDelay(r)
+	if delay := nextAllowedRequest.Sub(time.Now()); delay > 0 {
+		info := map[string]float64{"sleep_sec": delay.Seconds()}
+		return hh.ErrorJSON(http.StatusTooManyRequests, info) // return a 429 with JSON-encoded info
+	}
+	// OK!
+	json.NewEncoder(w).Write(thing)
+	return nil
 }
 ```
 
 Adding in errorware:
 
 ```go
-func slogHTTPErrors(r *http.Request, err error) error {
-	if err != nil {
+func slogHTTP(r *http.Request, err error) error {
+	if err == nil {
+		slog.DebugContext(r.Context(), "http request", "method", r.Method, "url", r.URL.String())
+	} else {
 		slog.ErrorContext(r.Context(), "http request failed", "method", r.Method, "url", r.URL.String(), "err", err)
 	}
 	return err
 }
 
-// All wrapped HTTP handlers will have their errors (nil or otherwise!) passed through slogHTTPErrors.
-mux := hh.WrapMux(http.NewServeMux(), slogHTTPErrors)
+// all handleThing errors (nil or otherwise) will be passed through slogHTTP
+mux.HandleFunc("GET /thing/{id}", hh.Wrap(srv.handleThing, slogHTTP)) // all handleThing errors
 ```
 
-Use for a single handler, without touching the mux:
+If this gets repetitive, use a closure:
 
 ```go
-subrouter.HandleFunc("/foo", hh.Wrap(srv.handleFoo)) // can also pass errorware to Wrap
+wrap := func(fn hh.HandlerFunc) http.HandlerFunc {
+	return hh.Wrap(fn, slogHTTP)
+}
 ```
-
 
 # What is it
 
@@ -85,7 +88,7 @@ if !authorized {
 // do sensitive stuff
 ```
 
-The secondary goals are minimality, compositionality, and "batteries included" APIs for core, common functionality.
+The secondary goals are minimality, compositionality, and just enough "batteries included" APIs for extremely common functionality.
 
 ## Non-goals
 
@@ -99,24 +102,32 @@ If there's clear consensus on common errorware, they might be a candidate.
 
 There are three parts to the package.
 
-1. Adapters for HTTP handlers that return errors.
-2. Special errors to trigger common HTTP responses.
-3. Hooks for "errorware": Inspecting and modifying errors on the way out.
+1. An adapter for HTTP handlers that return errors (`Wrap`).
+2. Special errors for common HTTP responses.
+3. Hooks for "errorware": Inspecting, modifying, replacing, or removing errors on the way out.
 
 These are all fundamentally interwoven, which is why they're all lumped together in one package.
 
-The adapters are `func Wrap`, which is the core low level adapter, and `type Mux`, which lets you avoid writing out `hh.Wrap` repeatedly, particularly when you're re-using a standard suite of errorware.
+### Wrap
 
-The core special error interface is `HTTPResponseError`, which gives total control over the HTTP response. There are helpers for the most common uses:
+The `Wrap` adapter converts handlers with errors to handlers. It buffers all responses written by wrapped handlers. This ensures that returning an error at any point is safe to do, because no output will have been written to the client. If buffering is undesirable for a particular endpoint, do not use hh for that endpoint.
+
+### Errors and responses
+
+The core special error interface is `HTTPResponseError`, which gives total control over the HTTP response. There is also a basic implementation, `ResponseError`, which supports sending a particular HTTP status code and text. (For more control over responses, such as setting Content-Type headers, create your own implementation to suit your needs.)
+
+There are helpers for the most common uses, implemented using `ResponseError`:
 
 * `Error` responds with the default text for the error code.
-* `ErrorText` responds with fixed text.
+* `ErrorText` responds with fixed text and an error code.
 * `Errorf` responds with fmt.Sprintf-formatted text.
 * `ErrorJSON` responds with JSON-encoded information.
 
 And a set of top level `Err*` errors for the most common errors (as determined by some highly scientific grepping).
 
-Both `Wrap` and `Mux` support integrated errorware, which is a way to log, inspect, and replace errors after the HTTP handler has finished processing.
+### Errorware
+
+`Wrap` supports integrated errorware, which is a way to log, inspect, and replace errors after the HTTP handler has finished processing.
 
 # License
 
@@ -128,4 +139,4 @@ Contributions are welcome, but I prioritize my life over open source maintainers
 
 # Acknowledgements
 
-Thank you to [Jonathan Hall](https://jhall.io) for excellent API design feedback.
+Thank you to [Jonathan Hall](https://jhall.io), [Joe Tsai](https://github.com/dsnet), and [David Crawshaw](https://crawshaw.io) for excellent API design feedback.
